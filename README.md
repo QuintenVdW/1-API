84dd0824bea811f18b779dea3848498e39fa8867
Quinten Van der Wildt <r0937889@student.thomasmore.be>
2023-11-10 17:21:05 +0100

    push start service?

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Als gekozen thema heb ik gekozen voor star wars characters en hun factie.
 Deze keuze heb ik gemaakt omdat ik een grote fan ben van star wars.
 
-## uitleg API
+## Uitleg API
 Met deze API kun je zoeken op naam van een star wars character om zo meer info over deze te krijgen.
 Je kan ook nieuwe characters of facties toevoegen bij deze database om deze zo te verbeteren. 
 
--- myproject/main.py
+++ myproject/main.py
@@ -42,7 +42,7 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 @app.get("/character/", response_model=schemas.Star_wars)
 def read_user(name: str, db: Session = Depends(get_db)):
-    db_user = crud.get_name(db, name=name)
+    db_user = crud.get_name(db, user_name=name)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
@@ -57,8 +57,8 @@ def create_user(user: schemas.Star_wars_create, db: Session = Depends(get_db)):
 
 
 @app.post("/faction_add/", response_model=schemas.Faction)
-def create_faction(user: schemas.FactionCreate, db: Session = Depends(get_db)):
-    faction = crud.get_faction_by_name(db, faction_name=user.name)
+def create_faction(faction: schemas.FactionCreate, db: Session = Depends(get_db)):
+    faction = crud.get_faction_by_name(db, faction_name=faction.name)
     if faction:
         raise HTTPException(status_code=400, detail="Name already registered")
     return crud.create_faction(db=db, faction=faction)
